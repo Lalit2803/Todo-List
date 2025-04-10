05199ec09acef2047bc7a61d4cb4031af3ee0d55
LALIT PRATAP SINGH <115670258+Lalit2803@users.noreply.github.com>
2025-04-10 11:15:51 +0530

    Update Login.jsx

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -27,7 +27,7 @@ export function LoginForm() {
     event.preventDefault();
   
     try {
-      const response = await fetch("http://localhost:9000/lps/user/login", {
+      const response = await fetch("https://todo-list-backend-deop.onrender.com/lps/user/login", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
